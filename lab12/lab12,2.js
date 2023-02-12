dc669e079cd30ef2587a5b4bb73e06585b8c2dff
BuckEliza <124238923+BuckEliza@users.noreply.github.com>
2023-02-12 21:21:27 +0600

    for ayub

--- lab12/lab12,2.js
+++
@@ -1,3 +0,0 @@
-function openWindow() {
-    window.open('history.html', 'History', 'width=600, height=400, left=500, top=200');
-}
---
+++ labs/lab12/lab12,2.js
@@ -0,0 +1,3 @@
+function openWindow() {
+    window.open("edge://history/all", "History", "width=600, height=450, left=100, top=50");
+}
